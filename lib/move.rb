bd6e7ac561eb41c4f61b06ec9e934f243dea58c8
Charity Youngblood <youngblood.charity@gmail.com>
2017-10-05 20:57:42 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,8 +8,8 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(user_input)
-  user_input.to_i
-  user_input - 1
+  new_user_input = user_input.to_i
+  new_user_input - 1
 end
 
 
